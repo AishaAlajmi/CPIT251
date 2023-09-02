6cafc1dce9c988efb7a9375a094344b75a95d80e
Aisha_Alajmi <105488603+AishaAlajmi@users.noreply.github.com>
2023-09-02 18:12:13 +0300

    Update CPIT251.java

--- src/CPIT251.java
+++ src/CPIT251.java
@@ -14,7 +14,7 @@ public class CPIT251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Aisha Alajmi - 2105522");    }
+        System.out.println("Aisha Alajmi - 2105522");    
     }
     
 }
