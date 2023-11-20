dcb64d11c9c70646a926bd44873a9a3999bc6f2b
Jamelah Hadi <108652057+Jamelah1420@users.noreply.github.com>
2023-11-20 19:52:07 +0300

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/User.java
+++ src/User.java
@@ -8,13 +8,6 @@
  *
  * @author aisha
  */
-public class CPIT251 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Aisha Alajmi - 2105522");    
-    }
+class User {
     
 }
